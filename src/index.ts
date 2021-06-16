495ea646baab4c607943e4189c6f5dff8204e63c
Jan Heise <jan@dasministerium.com>
2021-06-16 16:18:26 +0200

    Format

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import './assets/styles/index.css';
 import { App, Plugin } from 'vue';
 import * as components from './components';
 
-const install: Exclude<Plugin['install'], undefined> = function install(app: App) {
+const install: Exclude<Plugin['install'], undefined> = function install(
+  app: App
+) {
   Object.entries(components).forEach(([componentName, component]) => {
     app.component(componentName, component);
   });
